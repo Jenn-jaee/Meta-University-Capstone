550c58b75aa1874117aaf977ec4e1bff739bb735
Jennifer Obinwanne <jennobi@meta.com>
2025-07-17 11:42:25 -0700

    Add initial FeedPage component

---
+++ client/src/pages/FeedPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FeedPage.css';
+
+function FeedPage() {
+  return (
+    <div className="feed-page">
+      <h2 className="feed-heading">Your Feed</h2>
+      <p className="feed-placeholder">Posts from people you're connected to will appear here.</p>
+    </div>
+  );
+}
+
+export default FeedPage;
